bc1d1c3826931423208d9a614effc5f1e9099fb6
TechQuery <shiy007@qq.com>
2021-01-05 04:27:56 +0800

    [add] Check Box component [optimize] update Upstream packages

--- source/component/Example.tsx
+++ source/component/Example.tsx
@@ -1,10 +1,20 @@
 import { WebCellProps, createCell } from 'web-cell';
+import classNames from 'classnames';
 
 import style from './Example.less';
 
-export function Example({ defaultSlot }: WebCellProps) {
+export function Example({ className, defaultSlot, ...rest }: WebCellProps) {
     return (
-        <div className={`border border-light p-4 ${style.box}`}>
+        <div
+            className={classNames(
+                'border',
+                'border-light',
+                'p-4',
+                style.box,
+                className
+            )}
+            {...rest}
+        >
             {defaultSlot}
         </div>
     );
